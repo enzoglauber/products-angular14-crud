c9ebf9022d6c7fe27cc40d8d8295ff17d2c29962
Enzo Glauber de Lima Almeida <enzo.almeida@bradesco.com.br>
2022-10-16 19:15:56 -0300

    testing product filter

--- src/app/features/product/product-filter/product-filter.component.spec.ts
+++ src/app/features/product/product-filter/product-filter.component.spec.ts
@@ -10,10 +10,13 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 
+import { ProductService } from '../product.service';
+import { ProductFilter } from './product-filter';
 import { ProductFilterComponent } from './product-filter.component';
 
 describe('ProductFilterComponent', () => {
   let component: ProductFilterComponent;
+  let productService: ProductService;
   let fixture: ComponentFixture<ProductFilterComponent>;
 
   beforeEach(async () => {
@@ -37,11 +40,39 @@ describe('ProductFilterComponent', () => {
     .compileComponents();
 
     fixture = TestBed.createComponent(ProductFilterComponent);
+    productService = TestBed.inject(ProductService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should submited an empty form', () => {
+    jest.spyOn(productService, 'setFilter');
+    const value: ProductFilter = { category: '', code: '' };
+
+    component.submit();
+    expect(productService.setFilter).toHaveBeenCalledWith(value);
+  });
+
+  it('should submited with category filter', () => {
+    jest.spyOn(productService, 'setFilter');
+    const value: ProductFilter = { category: '1', code: '' };
+    component.form.patchValue(value);
+
+    component.submit();
+    expect(productService.setFilter).toHaveBeenCalledWith(value);
+  });
+
+
+  it('should submited with code filter', () => {
+    jest.spyOn(productService, 'setFilter');
+    const value: ProductFilter = { category: '', code: '22' };
+    component.form.patchValue(value);
+
+    component.submit();
+    expect(productService.setFilter).toHaveBeenCalledWith(value);
   });
 });
